7add489b04a63e494434e2054c1d6f0c60403256
William Santos <william.santos@conekta.com>
2021-08-06 13:26:48 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const validateBranchWorkflow = require('./validate-workflow')
 try {
   const targetBranch = github.context.base_ref
   const sourceBranch = github.context.head_ref
+  core.info('Target branch: ' + targetBranch)
+  core.info('Source branch: ' + sourceBranch)
   const validationResult = validateBranchWorkflow(targetBranch, sourceBranch)
   if (!validationResult) {
     core.setFailed('Source and Target branch does not satisfy gitflow workflow')
